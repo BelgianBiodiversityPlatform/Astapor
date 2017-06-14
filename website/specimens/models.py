7972d7c533a6b99f8cf9ee268d5d893f2cbeade3
Nicolas Noé <nicolas@niconoe.eu>
2017-06-14 15:04:24 +0200

    Better models.

--- website/specimens/models.py
+++ website/specimens/models.py
@@ -1,4 +1,22 @@
 from django.contrib.gis.db import models
 
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together = ("first_name", "last_name")
+
+class SpecimenLocation(models.Model):
+    name = models.CharField(max_length=100)
+
 class Specimen(models.Model):
+    specimen_id = models.IntegerField(unique=True)  # ID from the lab, not Django's PK
+    scientific_name = models.CharField(max_length=100)
     location = models.PointField()
+    depth = models.IntegerField(blank=True, null=True)
+    identified_by = models.ForeignKey(Person)
+    location = models.ForeignKey(SpecimenLocation)
+    comment = models.TextField(blank=True, null=True)
+
+
