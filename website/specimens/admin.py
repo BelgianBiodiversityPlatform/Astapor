a4756fd803aa0c3fc14a00287d0c133e150bf898
Nicolas Noé <nicolas@niconoe.eu>
2017-06-14 11:39:55 +0200

    Admin: Custom lat/lon widget instead of OL map.

--- website/specimens/admin.py
+++ website/specimens/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from specimens.models import Specimen
+
+from django import forms
+from .models import Specimen
+from .widgets import LatLongWidget
+
+
+# Custom form to provide lat/lon widget instead of OL map.
+class MyAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Specimen
+        fields = "__all__"
+        widgets = {
+            'location': LatLongWidget
+        }
+
 
 class SpecimenAdmin(admin.ModelAdmin):
-    pass
+    form = MyAdminForm
 
 admin.site.register(Specimen, SpecimenAdmin)
---
+++ website/specimens/widgets.py
@@ -0,0 +1,39 @@
+from django.forms import widgets
+from django.contrib.gis.geos import Point
+
+
+class LatLongWidget(widgets.MultiWidget):
+    """
+    A Widget that splits Point input into latitude/longitude text inputs.
+    """
+    template_name = 'django/forms/widgets/latlong.html'
+
+    def __init__(self, attrs=None):
+        _widgets = (
+            widgets.TextInput(attrs=attrs),
+            widgets.TextInput(attrs=attrs),
+        )
+
+        super(LatLongWidget, self).__init__(_widgets, attrs)
+
+    def get_context(self, name, value, attrs):
+        context = super(LatLongWidget, self).get_context(name, value, attrs)
+        context['lat_label'] = "Lat:"
+        context['long_label'] = "Lon:"
+        return context
+
+    def decompress(self, value):
+        if value:
+            return tuple(value.coords)
+        return (None, None)
+
+    def value_from_datadict(self, data, files, name):
+        mylat = data[name + '_0']
+        mylong = data[name + '_1']
+
+        try:
+            point = Point(float(mylat), float(mylong))
+        except ValueError:
+            return ''
+
+        return point
