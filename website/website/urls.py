e0109d4155ef2bf96dbb3a035a132186d82d4ecc
Nicolas Noé <nicolas@niconoe.eu>
2017-06-16 13:46:48 +0200

    Include django-export-action for easy CSV export

--- website/website/urls.py
+++ website/website/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^export_action/', include("export_action.urls", namespace="export_action")),
 ]
