2f3bc763628e634b399f006d919f782d5ffc8e27
Nicolas Noé <nicolas@niconoe.eu>
2017-07-12 16:12:02 +0200

    Specimens import script is now more verbose (#7)

--- website/specimens/management/commands/import_specimens.py
+++ website/specimens/management/commands/import_specimens.py
@@ -15,24 +15,6 @@ MODELS_TO_TRUNCATE = [Station, Expedition, Fixation, Person, SpecimenLocation, S
 # - Lat/lon use comma as a separator
 
 
-def get_or_create_station_and_expedition(station_name, expedition_name):
-    # Returns a Station object, ready to assign to Specimen.station
-    """
-
-    :rtype: Station
-    """
-    if station_name == '':
-        station_name = UNKNOWN_STATION_NAME
-
-    try: # A station already exists for the correct expedition?
-        return Station.objects.get(name=station_name, expedition__name=expedition_name)
-
-    except ObjectDoesNotExist:  # New station, let's create it (with expedition if needed):
-        expedition, _ = Expedition.objects.get_or_create(name=expedition_name)
-        station = Station.objects.create(name=station_name, expedition=expedition)
-        return station
-
-
 class Command(BaseCommand):
     help = 'Import specimens from a CSV file'
 
@@ -46,6 +28,28 @@ class Command(BaseCommand):
             default=False,
             help='Truncate specimens (and related) tables prior to import',
         )
+
+    def get_or_create_station_and_expedition(self, station_name, expedition_name):
+        # Returns a Station object, ready to assign to Specimen.station
+        """
+
+        :rtype: Station
+        """
+        if station_name == '':
+            station_name = UNKNOWN_STATION_NAME
+
+        try:  # A station already exists for the correct expedition?
+            return Station.objects.get(name=station_name, expedition__name=expedition_name)
+
+        except ObjectDoesNotExist:  # New station, let's create it (with expedition if needed):
+            expedition, created = Expedition.objects.get_or_create(name=expedition_name)
+            if created:
+                self.stdout.write(self.style.SUCCESS('Created new expedition: {0}...'.format(expedition)), ending='')
+
+            station = Station.objects.create(name=station_name, expedition=expedition)
+            self.stdout.write(self.style.SUCCESS('Created new station: {0}...'.format(station)), ending='')
+
+            return station
 
     def handle(self, *args, **options):
         self.stdout.write('Importing data from file...')
@@ -63,28 +67,44 @@ class Command(BaseCommand):
                 specimen.specimen_id = row['Specimen_id'].strip()
 
                 self.stdout.write('Specimen ID is {id}...'.format(id=specimen.specimen_id), ending='')
-                specimen.station = get_or_create_station_and_expedition(row['Station'].strip(), row['Expedition'].strip())
+                specimen.station = self.get_or_create_station_and_expedition(row['Station'].strip(),
+                                                                             row['Expedition'].strip())
 
                 # Identifiers
                 identified_by = row['Identified_by'].strip()
                 id_first_name, id_last_name = identified_by.split()
-                identifier, _ = Person.objects.get_or_create(first_name=id_first_name, last_name=id_last_name)
+                identifier, created = Person.objects.get_or_create(first_name=id_first_name, last_name=id_last_name)
+                if created:
+                    self.stdout.write(self.style.SUCCESS('Created new person: {0}...'.format(identifier)), ending='')
+
                 specimen.identified_by = identifier
 
                 # Specimen locations
-                specimen_location, _ = SpecimenLocation.objects.get_or_create(name=row['Specimen_location'])
+                specimen_location, created = SpecimenLocation.objects.get_or_create(name=row['Specimen_location'])
+                if created:
+                    self.stdout.write(self.style.SUCCESS('Created new Specimen Location: {0}...'.format(specimen_location)),
+                                      ending='')
+
                 specimen.specimen_location = specimen_location
 
                 # Coordinates
-                if row['Latitude'] and row['Longitude']:
+                latitude = row['Latitude'].strip()
+                longitude = row['Longitude'].strip()
+                if latitude and longitude:
                     lat = float(row['Latitude'].replace(',','.'))
                     lon = float(row['Longitude'].replace(',','.'))
                     specimen.coords = Point(lon, lat)
+                else:
+                    if latitude or longitude:
+                        raise CommandError('Either latitude or longitude is missing!')
 
                 # Fixation
                 fixation = row['Fixation'].strip()
                 if fixation:
-                    specimen.fixation, _ = Fixation.objects.get_or_create(name=fixation)
+                    specimen.fixation, created = Fixation.objects.get_or_create(name=fixation)
+                    if created:
+                        self.stdout.write(
+                            self.style.SUCCESS('Created new Fixation: {0}...'.format(specimen.fixation)),ending='')
 
                 specimen.comment = row['Comment']
 
