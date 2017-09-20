415237750e981dc1a2151ce01d90169ea0c2ce2e
Nicolas Noé <nicolas@niconoe.eu>
2017-09-20 15:18:50 +0200

    Added a command to perform the whole data import/populate the webapp. Fix #26.

---
+++ website/specimens/management/commands/full_import.py
@@ -0,0 +1,25 @@
+from django.core import management
+
+from ._utils import AstaporCommand
+
+
+class Command(AstaporCommand):
+    help = 'Call other commands in sequence to perform the full data import and initial processing.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('specimen_csv_file')
+        parser.add_argument('taxonomy_csv_file')
+
+    def handle(self, *args, **options):
+        self.w('1. Importing specimens')
+        management.call_command('import_specimens', '--truncate', '{f}'.format(f=options['specimen_csv_file']))
+
+        self.w('2. Importing taxonomy')
+        management.call_command('import_taxonomy', '--truncate', '{f}'.format(f=options['taxonomy_csv_file']))
+
+        self.w('3. Reconcile taxonomy')
+        management.call_command('reconcile_taxonomy', '--all')
+
+        self.w('4. Interpret capture dates')
+        management.call_command('interpret_capture_dates', '--all')
+
